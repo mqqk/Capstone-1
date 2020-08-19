f8b6460ab9018f9de401a505255df52e8ec3c702
Mick Styers <68083989+mqqk@users.noreply.github.com>
2020-08-18 23:38:23 -0400

    stylized mobile first on the opening page

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,20 +7,27 @@
 //function to provide key terms to DOM 
 function keyTerms(){
     $('#loadOut').append(`
-    <div>
-    <ul id="keylist"><h2> Key Terms List</h2>
-        <li><a class="contactFont" id="js-nibrs" href="#">NIBRS</a>  </li>
-        <li><a class="contactFont" id="js-ucr" href="#">UCR</a></li>
-        <li> <a class="contactFont" id="js-ori" href="#">ORI</a></li>
-        <li> <a class="contactFont" id="js-ncic" href="#">NCIC</a></li>
-    </ul>
-    <div id="definition"></div>
+    
+    <div id="keylist"><h2> Key Terms List</h2>
+        <a class="contactFont" id="js-nibrs" href="#">NIBRS</a><br>  
+        <a class="contactFont" id="js-ucr" href="#">UCR</a><br>
+         <a class="contactFont" id="js-ori" href="#">ORI</a><br>
+         <a class="contactFont" id="js-ncic" href="#">NCIC</a><br>
+         <button type='submit' id='backMain'>Back</button>
     </div>
+    <div id="definition"></div>
+    
     `)
+    $('#backMain').on('click',function(e){
+        event.preventDefault();
+        $('#loadOut').empty();
+        $('#nav').show();})
+
     $('#js-nibrs').on('click',function(e){
+        event.preventDefault();
         $('#keylist').hide();
         $('#definition').append(`<p>
-        NIBRS</p> <br><p>-National Incident-Based Reporting System</p><br><p> -captures detail on each single crime incident—as well as on separate offenses within the same incident</p><br><p>-This includs information on victims, known offenders, relationships between victims and offenders, arrestees, and property involved in crimes.</p>
+        NIBRS</p> <p>-National Incident-Based Reporting System</p><p> -captures detail on each single crime incident—as well as on separate offenses within the same incident</p><p>-This includs information on victims, known offenders, relationships between victims and offenders, arrestees, and property involved in crimes.</p>
         <button type='submit' id='backButt'>Back</button>`)
         $('#backButt').on('click',function(e){
             $('#definition').empty();
@@ -30,6 +37,7 @@ function keyTerms(){
     })
 
     $('#js-ori').on('click',function(e){
+        event.preventDefault();
         $('#keylist').hide();
         $('#definition').append(`<p>
         ORI</p><br><p> - Originating Reporting Agency Identifier</p><br><p> - Every agency that reports to the UCR is assigned a unique ORI.</p>
@@ -42,6 +50,7 @@ function keyTerms(){
     })
 
     $('#js-ucr').on('click',function(e){
+        event.preventDefault();
         $('#keylist').hide();
         $('#definition').append(`<p>
         UCR</p><br><p> - Uniform Crime Reporting</p><br><p> - program file containing 18,725 records, one for each law enforcement agency in the Nation that had ever reported to the UCR as of 1996.</p>
@@ -54,6 +63,7 @@ function keyTerms(){
     })
 
     $('#js-ncic').on('click',function(e){
+        event.preventDefault();
         $('#keylist').hide();
         $('#definition').append(`<p>
         NCIC</p><br><p> - National Crime Information Center</p><br><p> -Established in 1967, NCIC is a nationwide computerized system that provides law enforcement with ready data about wanted persons,  stolen property, and other information.</p>
@@ -70,13 +80,18 @@ function keyTerms(){
 function tools(){
     $('#loadOut').append(`
     <div>
-    <ul> Search Tools
-        <li>-There are two search options. The first is an arrest total by each state in a given year for particular crimes. You can query a second city which will populate another column for comparison. </li>
-        <li>-The second tool provides a summary of crimes reported by each jurisdiction and whether these crimes were cleared.</li>
-        <li> -There are more search tools planned.</li>
-    </ul>
+        <h2>Search Tools</h2>
+        <p>-There are two search options. The first is an arrest total by each state in a given year for particular crimes. You can query a second state and year which will populate another column for comparison. </p>
+        <p>-The second tool provides a summary of crimes reported by each jurisdiction and whether these crimes were cleared. You may query a second jurisdiction and year, however, keep in mind that the data is not sorted and the FBI reports the data as it is provided from the jurisdiction. The report patterns are not consistent.</p>
+        <p> -There are more search tools planned.</p>
+        <button type='submit' id='backMain'>Back</button>
+
     </div>
     `)
+    $('#backMain').on('click',function(e){
+        event.preventDefault();
+        $('#loadOut').empty();
+        $('#nav').show();})
 }
 
 
@@ -91,20 +106,35 @@ function recentSearches(state,year){
     `)
 }
 
-function loadUp(){
-        
-    $('#keyTerms').click(event =>{
+function hideForTools(){
+    $('#tools').click(event=>{
+        event.preventDefault();
         $('#loadOut').empty();
-        keyTerms();
-    })
-    $('#tools').click(event =>{
-        $('#loadOut').empty();
+        $('#nav').hide();
         tools();
     })
+}
+
+//hides and clears for keyTerms to run
+function hideForKeyTerms(){
+    $('#keyTerms').click(event =>{
+        event.preventDefault();
+        console.log('running hide nav');
+        $('#loadOut').empty();
+        $('#nav').hide();
+        keyTerms();
+    })
+
+}
+
+function loadUp(){
+    hideForKeyTerms();
+    hideForTools(); 
+    
     $('#recentSearches').click(event =>{
         $('#loadOut').empty();
         recentSearches();
     })
 }
 
-$(loadUp())
+$(loadUp());
